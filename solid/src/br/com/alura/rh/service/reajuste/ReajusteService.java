f17b1d0a901259f401cb3db94ac33332a894cac9
Ivanildo Junior <ivanildo@outlook.com>
2023-06-13 20:31:26 -0300

    chore. refactor packages

--- solid/src/br/com/alura/rh/service/reajuste/ReajusteService.java
+++ solid/src/br/com/alura/rh/service/reajuste/ReajusteService.java
@@ -1,6 +1,5 @@
-package br.com.alura.rh.service;
+package br.com.alura.rh.service.reajuste;
 
-import br.com.alura.rh.ValidacaoException;
 import br.com.alura.rh.model.Funcionario;
 
 import java.math.BigDecimal;
