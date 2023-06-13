58ffa7a54da198f40ce783b0a93802525889f1bc
Ivanildo Junior <ivanildo@outlook.com>
2023-06-13 20:14:42 -0300

    feat. implemented validators

--- solid/src/br/com/alura/rh/service/ReajusteService.java
+++ solid/src/br/com/alura/rh/service/ReajusteService.java
@@ -4,30 +4,23 @@ import br.com.alura.rh.ValidacaoException;
 import br.com.alura.rh.model.Funcionario;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 public class ReajusteService {
 
+    private List<ValidacaoReajuste> validacoes;
+
+    public ReajusteService(List<ValidacaoReajuste> validacoes) {
+        this.validacoes = validacoes;
+    }
+
     public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento){
-        BigDecimal salarioAtual = funcionario.getSalario();
-        BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
-        if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-            throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-        }
 
-        LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
-        LocalDate dataAtual = LocalDate.now();
+        this.validacoes.forEach(v -> v.validar(funcionario, aumento));
 
-        long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
-
-        if(mesesDesdeUltimoReajuste < 6){
-            throw new ValidacaoException("Intervalo entre reajustes deve ser de no minimo 6 meses");
-        }
-
-        BigDecimal salarioReajustado = salarioAtual.add(aumento);
+        BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
         funcionario.reajustarSalario(salarioReajustado);
+
     }
 
 }
