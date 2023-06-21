78ad05bb55648522a6b6ff9bdf403ecfa2dfcbd2
Ivanildo Junior <ivanildo@outlook.com>
2023-06-21 20:48:24 -0300

    feat. added pattern command handler

---
+++ desing patterns I/src/br/com/alura/loja/pedido/GeraPedidoHandler.java
@@ -0,0 +1,20 @@
+package br.com.alura.loja.pedido;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.time.LocalDateTime;
+
+public class GeraPedidoHandler {
+
+    //contrutor com injeção de dependências: repository, service, etc
+
+    public void execute(GeraPedido dados){
+        Orcamento orcamento = new Orcamento(dados.getValorOrcamento(), dados.getQuantidadeItens());
+
+        Pedido pedido = new Pedido(dados.getCliente(), LocalDateTime.now(), orcamento);
+
+        System.out.println("Salva no banco");
+        System.out.println("Enviar email");
+    }
+
+}
