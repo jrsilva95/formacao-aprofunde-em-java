f1335da8be7f3a15a07832ee53cfd6343787b91b
Ivanildo Junior <ivanildo@outlook.com>
2023-07-01 18:06:47 -0300

    feat. added patter composite

---
+++ desing patterns II/src/br/com/alura/loja/TestesImposto.java
@@ -0,0 +1,21 @@
+package br.com.alura.loja;
+
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.imposto.ISS;
+import br.com.alura.loja.orcamento.ItemOrcamento;
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class TestesImposto {
+
+    public static void main(String[] args) {
+        Orcamento orcamento = new Orcamento();
+        orcamento.adicionarItem(new ItemOrcamento(new BigDecimal("100")));
+        CalculadoraDeImpostos calculadoraDeImpostos = new CalculadoraDeImpostos();
+
+        System.out.println(calculadoraDeImpostos.calcular(orcamento, new ISS(new ICMS(null))));
+
+    }
+}
